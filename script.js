ce74f2cf5fb5acda11507249ca8973f53018f67d
tulam3112 <hoangtulam3112@gmail.com>
2025-02-26 09:15:23 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function login()
+{
+  let a = username.value
+  let b = password.value
+  if (a == "lam" && b =="17")
+  {
+    window.open("home.html")
+    document.gêtlementById(compiler).style.đisplay="block"
+  }
+  else document.write("Không đúng")
+}
